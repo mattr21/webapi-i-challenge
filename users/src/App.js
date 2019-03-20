6ac67359beca90a8ac5b7a69e6125d1fa384b48c
mattr21 <matt.rothstein21@gmail.com>
2019-03-19 22:05:32 -0700

    created a UsersList component and added a NavLink and Route to that component in App.js

--- users/src/App.js
+++ users/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import { Route, NavLink } from 'react-router-dom';
+import UsersList from './components/UsersList.js';
 
 
 class App extends Component {
@@ -10,7 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>test</p>
+        {/* Links */}
+        <NavLink to="/"><button>Home</button></NavLink>
+
+        {/* Routes */}
+        <Route exact path='/' render={ props => <UsersList {...this.state} {...props} /> } /> 
+
+        {/* <Route exact path="/" render={ props => <FriendsList {...this.state} {...props} deleteFriend={this.deleteFriend} setUpdateForm={this.setUpdateForm} /> } /> */}
       </div>
     );
   }
---
+++ users/src/components/UsersList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UsersList = props => {
+    return (
+        <div>test from UsersList</div>
+    )
+}
+
+export default UsersList;
